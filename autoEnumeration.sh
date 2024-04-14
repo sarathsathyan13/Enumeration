ea37c30459ca64c632a2c7a34f8bb882bfe3bc0d
SARA <84668724+sarathsathyan13@users.noreply.github.com>
2024-04-14 11:47:36 +0000

    Update autoEnumeration.sh
    file updated

--- autoEnumeration.sh
+++ autoEnumeration.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
+# Check if a domain name is provided as an argument
+if [ -z "$1" ]; then
+  echo "Invalid Syntax. Please Provide Domain Name."
+  echo "Eg: $0 example.com"
+  exit 1
+fi
+
 word=$1
 
-#argument check
-if [[ -z $word ]]; then
-	echo "Invalid Syntax.Please Provide Domain Name."
-	echo "Eg: $0 example.com"
-else
+# Run assetfinder
+echo "Starting Assetfinder"
+echo "=============="
+assetfinder --subs-only "$word" | tee -a domains.txt
 
-	#running assetfinder
-	echo "Starting Assetfinder"
-	echo "=============="
-	assetfinder --subs-only $1 | tee -a domains.txt
+# Remove duplicate entries
+sort -u domains.txt -o domains.txt
 
-	#removing duplicate entries
-	sort -u domains.txt -o domains.txt
+# Check for alive domains
+echo "Checking For Alive Domains"
+echo "=============="
+cat domains.txt | httprobe | tee -a alive.txt
 
-	#checking for alive domains
-	echo "Checking For Alive Domains"
-	echo "=============="
-	cat domains.txt | httprobe | tee -a alive.txt
+# Remove duplicate and sort
+echo "Sorting Domains"
+echo "=============="
+while read url; do
+  echo "${url#*//}" >> alive_subs.txt
+done < alive.txt
+sort -u alive_subs.txt -o sorted_alive_subs.txt
+count=$(wc -l < sorted_alive_subs.txt)
 
-	#removing duplicate and sorting
-	echo "Sorting Domains"
-	echo "=============="
-	while read url; do
-		echo ${url#*//} >> alive_subs.txt
-	done < alive.txt
-	sort -u alive_subs.txt -o sorted_alive_subs.txt
-	count=$(cat sorted_alive_subs.txt | wc -l)
+# Execution completed
+echo "Script Execution Completed"
+echo "Total ${count} Subdomains Found"
 
-	#execution completed
-	echo "Script Execution Completed"
-	echo "Total ${count} Subdomains Found"
-
-	#remove unwnated files
-	rm domains.txt
-	rm alive.txt
-	rm alive_subs.txt
-fi
+# Remove unwanted files
+rm domains.txt
+rm alive.txt
+rm alive_subs.txt
